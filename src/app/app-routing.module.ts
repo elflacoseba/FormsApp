30a6a050394aac2f6564bb926ca849688f230663
elflacoseba <sebaserri@gmail.com>
2024-03-02 20:41:15 -0300

    Agrega rutas y componentes para la autenticación y la reactividad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,20 @@ import { Routes, RouterModule } from '@angular/router';
 // Importa los componentes que deseas utilizar en las rutas
 
 // Define las rutas de tu aplicación
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then(m => m.ReactiveModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
