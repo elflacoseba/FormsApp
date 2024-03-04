87022df71a368f18dd3f77673147d1c989e54c84
elflacoseba <sebaserri@gmail.com>
2024-03-04 18:30:53 -0300

    Elimino un objeto del FormArray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -52,6 +52,10 @@ export class DynamicPageComponent {
     ])
   });
 
+  onDeleteFavorite( index: number ): void {
+    this.favoriteGames.removeAt(index);
+  }
+
   onSubmit(): void {
 
     if ( this.myForm.invalid ) {
