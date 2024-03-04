5fff93edd6eaf16ee7f1fb07c9639c760f0075df
elflacoseba <sebaserri@gmail.com>
2024-03-04 18:27:42 -0300

    Validaciones en formularios dinámicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -12,6 +12,38 @@ export class DynamicPageComponent {
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
+
+  constructor( private fb: FormBuilder ) { }
+
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+
+    return null;
+  }
+
+  isValidField( field: string ) {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ): boolean | null {
+    return formArray.controls[index].errors
+      && formArray.controls[index].touched;
+  }
+
   public myForm = this.fb.group({
     name: ['', [ Validators.required, Validators.minLength(3) ]],
     favoriteGames: this.fb.array([
@@ -19,8 +51,6 @@ export class DynamicPageComponent {
       ['Death Stranding', Validators.required],
     ])
   });
-
-  constructor( private fb: FormBuilder ) { }
 
   onSubmit(): void {
 
