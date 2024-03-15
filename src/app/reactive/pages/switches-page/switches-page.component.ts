e111d91253e5c66ef562fdd795c914eaceca3a3b
elflacoseba <sebaserri@gmail.com>
2024-03-15 16:13:18 -0300

    Agrega validación y funcionalidad de formulario de switches

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class SwitchesPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    gender: [ 'M', Validators.required ],
+    wantNotifications: [ true, Validators.required ],
+    termsAndConditions: [ false, Validators.requiredTrue ]
+  })
+
+  constructor( private fb: FormBuilder ){  }
+
+  isValidField( field: string ): boolean {
+    if ( this.myForm.controls[field]?.errors
+      && this.myForm.controls[field]?.touched ) {
+      return true;
+    }
+      else {
+        return false;
+    }
+  }
+
+  onSave() {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 }
