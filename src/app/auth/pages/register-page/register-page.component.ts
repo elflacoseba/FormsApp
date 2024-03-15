b86d67339e4c3f6f289486711fdefc8a37d8756f
elflacoseba <sebaserri@gmail.com>
2024-03-15 17:05:28 -0300

    Añadir validaciones y formulario reactivo en la página de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../shared/services/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+public myForm: FormGroup = this.fb.group({
+  name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern ) ]],
+  email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern) ]],
+  username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
+  password: ['', [ Validators.required, Validators.minLength(6) ]],
+  password2: ['', [ Validators.required ]]
+});
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+    ) { }
+
+  isValidField( field: string ): boolean | null {
+  //TODO: obtener validación desde un servicio
+    return this.validatorsService.isValidField( field, this.myForm );
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
